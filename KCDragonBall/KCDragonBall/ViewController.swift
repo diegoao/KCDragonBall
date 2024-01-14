bb451a061c331a8d01d28e227af6b1ffdb5598f5
Diego Andrades <die_94_go@hotmail.com>
2024-01-14 09:16:18 +0100

    Creo nuevo proyecto para empezar con la práctica

---
+++ KCDragonBall/KCDragonBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KCDragonBall
+//
+//  Created by Macbook Pro on 14/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
